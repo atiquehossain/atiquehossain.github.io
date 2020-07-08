d01bd8e5f2ff516f78d8c644866518b88668f96a
Atique <atique.hossain@northsouth.edu>
2020-07-08 06:32:17 +0600

    menu fixed

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,8 +10,22 @@ _gaq.push(['_setAccount', 'UA-36251023-1']);
 _gaq.push(['_setDomainName', 'jqueryscript.net']);
 _gaq.push(['_trackPageview']);
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  //ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+
+
+$(document).ready(function() {
+           $(".menu-icon").on("click", function() {
+                 $("nav ul").toggleClass("showing");
+           });
+     });
+
+     // Scrolling Effect
+
+     $(window).on("scroll", function() {
+           if($(window).scrollTop()) {
+                 $('nav').addClass('black');
+           }
+
+           else {
+                 $('nav').removeClass('black');
+           }
+     })
